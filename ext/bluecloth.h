6f540d08a87b579c3d9264a2e722ba12ef5f44f0
Marcus Krejpowicz <krebbl@users.noreply.github.com>
2022-03-31 14:47:59 +0200

    reorder inclusion to fix compilation error

--- ext/bluecloth.h
+++ ext/bluecloth.h
@@ -7,19 +7,20 @@
 #ifndef BLUECLOTH_H
 #define BLUECLOTH_H
 
-#include "config.h"
-#include "assert.h"
-
-#include "mkdio.h"
 #include "ruby.h"
-
-void mkd_initialize  		_(( void ));
-void mkd_with_html5_tags	_(( void ));
 
 #if defined(HAVE_RUBY_ENCODING_H) && HAVE_RUBY_ENCODING_H
 #	define M17N_SUPPORTED
 #	include "ruby/encoding.h"
 #endif
+
+#include "config.h"
+#include "assert.h"
+
+#include "mkdio.h"
+
+void mkd_initialize  		_(( void ));
+void mkd_with_html5_tags	_(( void ));
 
 /* Replace the macro from encoding.h that refers to static 'rb_encoding_list' */
 #ifdef ENC_FROM_ENCINDEX
